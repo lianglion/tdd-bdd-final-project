baeb82ae3f36915a4792f3faeccee469edd8fe33
lianglion <liang.l.li@hotmail.com>
2025-02-18 16:19:41 +0800

    Update web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -104,7 +104,19 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element(By.ID, button_id).click()
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
 
 ##################################################################
 # This code works because of the following naming convention:
