1c73bb09b96c17d2261bd3bbe44c70f229c08c9a
lianglion <liang.l.li@hotmail.com>
2025-02-18 14:30:02 +0800

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -43,24 +43,7 @@ DATABASE_URI = os.getenv(
 )
 BASE_URL = "/products"
 
-######################################################################
-# READ A PRODUCT
-######################################################################
-@app.route("/products/<int:product_id>", methods=["GET"])
-def get_products(product_id):
-    """
-    Retrieve a single Product
 
-    This endpoint will return a Product based on it's id
-    """
-    app.logger.info("Request to Retrieve a product with id [%s]", product_id)
-
-    product = Product.find(product_id)
-    if not product:
-        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
-
-    app.logger.info("Returning product: %s", product.name)
-    return product.serialize(), status.HTTP_200_OK
 
 ######################################################################
 #  T E S T   C A S E S
